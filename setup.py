7a1e3489184676e92153d17dd5fdbe02d35aae99
Harpreet Singh <singhh@tcd.ie>
2016-08-22 10:27:25 -0700

    fixed setup

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='hedera',
+    name='proactiveHadoop',
     version='0.0.0',
-    description='Implementation of Hedera Scheduling System',
-    author='Ian Walsh, Anh Truong',
-    author_email='iwalsh, anhlt92@stanford.edu',
+    description='Implementation of proactive Scheduling System for Hadoop',
+    author='Harpreet Singh',
+    author_email='singhh@tcd.ie',
     packages=find_packages(exclude='test'),
     long_description="""\
-For Stanford CS244 Spring 2015 lab 3. Build on top of Brandon Heller's RIPL and RIPLPOX libraries
+MSc CS Dissertation project submitted to Trinity College Dublin, 2016
       """,
       classifiers=[
           "License :: OSI Approved :: GNU General Public License (GPL)",
